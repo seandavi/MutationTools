6220b150101c1501d00b068aedf6f651bc3bef23
Sean Davis <seandavi@gmail.com>
2014-03-19 06:42:11 -0400

    Added VRanges as potential input to isSNV

--- R/isSNV.R
+++ R/isSNV.R
@@ -3,7 +3,7 @@
 #' Returns TRUE for variants that are SNVs and FALSE otherwise.
 #' For variants with multiple ALT alleles, only the FIRST is used.
 #'
-#' @param VCF an object inheriting from the VCF class
+#' @param variants an object inheriting from the \code\link{VCF}} or \code{\link{VRanges}} classes
 #' @return logical vector with the same length as \code{vcf}
 #' @keywords character
 #' @seealso \code{\link{VCF-class}}
@@ -14,10 +14,20 @@
 #' param <- ScanVcfParam(fixed="ALT", geno=c("GT", "GL"), info=c("LDAF"))
 #' vcf = readVcf(fl,"hg19",param=param)
 #' table(isSNV(vcf))
-isSNV <- function(vcf) {
-    if(!inherits(vcf,'VCF')) {
-        stop('The isSimpleVariant function needs an object that inherits from the VCF class')
+isSNV <- function(variants) {
+
+    if(inherits(variants,'VCF')) {
+        refall = as.character(ref(variants))
+        altall = as.character(unlist(alt(variants)[start(PartitioningByEnd(alt(variants)))]))
+        return((nchar(refall)==1) & (nchar(altall)==1))
     }
-    altall = unlist(alt(vcf)[start(PartitioningByEnd(alt(vcf)))])
-    return((width(ref(vcf))==1) & (width(altall)==1))
+
+    if(inherits(variants,'VRanges')) {
+        refall = as.character(ref(variants))
+        altall = as.character(alt(variants))
+        return((nchar(refall)==1) & (nchar(altall)==1))
+    }
+
+    stop('parameter variants must be a VRanges or VCF object')
+
 }
